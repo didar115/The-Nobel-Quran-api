c0a79c0ab020c6f3b31708c95c4131f0c304c94e
didar <dider.alam115@gmail.com>
2021-09-03 17:56:53 +0600

    :hammer: bottom padding added

--- main.js
+++ main.js
@@ -32,7 +32,6 @@ const getApiData = () => {
 		const url = `https://api.quran.com/api/v4/chapters?language=en`;
 		fetch(url)
 			.then((res) => res.json())
-
 			.then((data) => showSearchData(data.chapters));
 			// .then((data) => showSearchData(data.docs, searchText, data.numFound));
 	// }
@@ -50,7 +49,6 @@ const showSearchData = (books) => {
 
 	showItems.innerHTML = "";
 	header.classList.remove("d-none");
-	// spinner.classList.add("d-none");
 
 	// implementing for Each coz api data we found as an array
 	books.forEach((element) => {
@@ -62,18 +60,19 @@ const showSearchData = (books) => {
 		newDiv.innerHTML = `
             <div class="custom-card" onclick="getSurahDetails('${element.id}','${element.name_simple}','${element.translated_name.name}')" >
                 <div class="card-body d-flex justify-content-around align-items-center">
-				    <div class="col-2" id="surah-id"><h5 class="card-title text-light text-center mt-2" >${element.id}</h5></div>
+							
+				   		 <div class="col-2" id="surah-id"><h5 class="card-title text-light text-center mt-2" >${element.id}</h5></div>
 
-                    <div class="col-6 ps-3">
-                    	<div>
-							<h5 class="card-title text-light fw-bold">${element.name_simple}</h5>
-							<small class="text-secondary">${element.translated_name.name}</small>
+						<div class="col-6 ps-3">
+							<div>
+								<h5 class="card-title text-light fw-bold">${element.name_simple}</h5>
+								<small class="text-secondary">${element.translated_name.name}</small>
+							</div>
 						</div>
-                     </div>
 
-                     <div class="col-4">
-                         <h6 class="card-title text-info fs-3" id="arabic-name">${element.name_arabic}</h6>
-                     </div>
+						<div class="col-4">
+							<h6 class="card-title text-info fs-3" id="arabic-name">${element.name_arabic}</h6>
+						</div>
 				
                     
                 </div>
@@ -86,16 +85,16 @@ const showSearchData = (books) => {
 	
 }
 
-const getSurahDetails = (id, name, eng_name) => {
+const getSurahDetails = (id, name,eng_name) => {
 	console.log("hello", id);
 		url = `https://api.quran.com/api/v4/quran/verses/uthmani?chapter_number=${id}`;
 		fetch(url)
 			.then(res => res.json())
-			.then(data => getSurahText(data.verses,name,eng_name));
+			.then(data => getSurahText(data.verses,id,name,eng_name));
 };
 	
 
-const getSurahText = (text,name,eng_name) => {
+const getSurahText = (text,id,name,eng_name) => {
 	
 	console.log(text);
 	console.log(name);
@@ -105,27 +104,26 @@ const getSurahText = (text,name,eng_name) => {
 	showItems.innerHTML = `
 	<div class="bg-secondary text-light mt-5 p-2 d-flex justify-content-evenly align-items-center">
 	<div>
-	<button class="btn btn-warning" onclick="getApiData()">Back</button></div>
+	<button class="btn btn-warning" onclick="getApiData()">Back</button>
+	</div>
 	
 		<div>
-		<h3>Surah ${name}</h3>
-		<h4>${eng_name} </h4>
+		<h3>${id}  Surah ${name}</h3>
+		<h6>${eng_name} </h6>
 		</div>
 	 
 	<h3 class="text-light"> Verse: ${text.length} </h3>
 	</div>
 	`;
+
 	text.forEach(element => {
 
 		const newDiv=document.createElement("div");
 		newDiv.innerHTML = `
-
 		
-
 		<h1 class="text-light text-end"> ${element.text_uthmani}</h1>
 		<hr style="color:white; height:1px">
 		`;
-		// console.log(element);
 		showItems.appendChild(newDiv);
 	});
 	
